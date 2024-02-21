a81cf712e62a0130a130bab41a84413638ec6189
Selim <selimsevim1@gmail.com>
2024-02-22 00:13:03 +0100

    initial-push

--- de_import_ease/settings.py
+++ de_import_ease/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
